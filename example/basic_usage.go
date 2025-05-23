e836d2d8de18ce019728c1dc8aad13374b96db66
Alex Konyukov <alexander.konyukov@behavox.com>
2025-05-22 21:13:05 -0400

    feat: add test app and bash script to start vector

---
+++ example/basic_usage.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	go_vector_logger "go-vector-logger"
+)
+
+func main() {
+
+	log, err := go_vector_logger.New("test-app", "INFO", "127.0.0.1", 6000)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello, World!\n")
+
+	log.Debug("test debug message")
+	log.Info("test info message")
+	log.Warn("test warning message")
+	log.Error("test error message")
+	log.Errorf("test error message with %s", "formatting")
+	log.Fatal("test error message")
+	log.Fatalf("test error message with %s", "formatting")
+	log.FatalError(err)
+}
